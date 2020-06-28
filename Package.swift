94de86b4e328f545cafbd05c9b752c4e308527cb
Danny Gilbert <spydercapriani@gmail.com>
2020-06-28 01:35:32 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "SpreadsheetView",
+    products: [
+        .library(
+            name: "SpreadsheetView",
+            targets: ["SpreadsheetView"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SpreadsheetView",
+            dependencies: [])
+    ]
+)
